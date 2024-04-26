1088166f52fc78c62a99f1cba2e9ea640294c68a
Allard Oelen <oelen@l3s.de>
2024-04-26 14:36:57 +0200

    fix: add missing sentence ending

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <main>
       <div className="container flex justify-center items-center mt-10 box-white !py-10">
         <h1 className="text-4xl pe-5">404</h1>{' '}
-        <span>This page cannot be found</span>
+        <span>This page cannot be found.</span>
       </div>
     </main>
   );
