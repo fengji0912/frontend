392c135467843aa8838427e97aa2394fdc0f6ea9
Allard Oelen <oelen@l3s.de>
2024-05-12 02:21:21 +0200

    fix: dark mode for cookie notification

--- src/app/(layout)/CookieNotification/CookieNotification.tsx
+++ src/app/(layout)/CookieNotification/CookieNotification.tsx
@@ -12,20 +12,17 @@ export default function CookieNotification() {
 
   return !isCookieWarningDismissed ? (
     <div
-      className="w-72 bg-white rounded-3xl shadow-box p-6 fixed bottom-4 right-4"
+      className="w-72 box-white rounded-3xl shadow-box p-6 fixed bottom-4 right-4"
       style={{ cursor: 'auto' }}
     >
       <div className="flex justify-center -mt-10 mb-3">
         <FontAwesomeIcon icon={faCookieBite} size="4x" color="#865700" />
       </div>
-      <span className="w-full block leading-normal text-gray-800 text-md mb-3">
+      <span className="w-full block leading-normal text-md mb-3">
         We use cookies to provide a better user experience.
       </span>
       <div className="flex items-center justify-between">
-        <Link
-          href={ROUTES.DATA_PROTECTION}
-          className="text-xs text-gray-400 mr-1 hover:text-gray-800"
-        >
+        <Link href={ROUTES.DATA_PROTECTION} className="text-xs  mr-1 ">
           Data Protection
         </Link>
         <CookieAcceptButton />
