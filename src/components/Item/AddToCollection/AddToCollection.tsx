5850a7c205756c155bdd29ae6132e225b8fccfca
Allard Oelen <oelen@l3s.de>
2024-05-30 11:02:15 +0200

    feat: select first collection when bookmarking items
    Fixes #6

--- src/components/Item/AddToCollection/AddToCollection.tsx
+++ src/components/Item/AddToCollection/AddToCollection.tsx
@@ -6,7 +6,13 @@ import {
   Selection,
   SelectItem,
 } from '@nextui-org/react';
-import { ReactElement, useEffect, useState, useTransition } from 'react';
+import {
+  ReactElement,
+  useCallback,
+  useEffect,
+  useState,
+  useTransition,
+} from 'react';
 import useSWR from 'swr';
 
 import getCollections from '@/app/my-library/[[...collectionId]]/helpers/getCollections';
@@ -24,6 +30,9 @@ export default function AddToCollection({
   itemId,
   trigger,
 }: AddToCollectionProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedKeys, setSelectedKeys] = useState<Selection | null>(null);
+
   const { data: collections } = useSWR('collections', () => getCollections());
 
   const { data, mutate } = useSWR('bookmarked' + itemId, () =>
@@ -31,57 +40,96 @@ export default function AddToCollection({
   );
   const bookmarkCollectionItems = data?.items;
   const [isPending, startTransition] = useTransition();
-  const [selectedKeys, setSelectedKeys] = useState<Selection>();
 
-  const handleSelectionChange = (keys: Selection) => {
-    setSelectedKeys(keys);
-    startTransition(async () => {
-      await bookmarkItem({ itemId, collectionId: [...keys][0] as string });
-      mutate();
-    });
-  };
+  const handleSelectionChange = useCallback(
+    (keys: Selection) => {
+      setSelectedKeys(keys);
+      startTransition(async () => {
+        await bookmarkItem({ itemId, collectionId: [...keys][0] as string });
+        mutate();
+      });
+    },
+    [itemId, mutate]
+  );
 
   useEffect(() => {
-    setSelectedKeys(
-      new Set(
-        bookmarkCollectionItems && bookmarkCollectionItems.length > 0
-          ? bookmarkCollectionItems?.map((item) => item.expand!.collection!.id)
-          : []
-      )
-    );
-  }, [bookmarkCollectionItems]);
+    // set default selected collection if no collection is selected and popover is opened
+    if (
+      isOpen &&
+      bookmarkCollectionItems &&
+      bookmarkCollectionItems.length === 0 &&
+      collections &&
+      collections.length > 0 &&
+      selectedKeys === null
+    ) {
+      const newSelection = new Set([collections[0].id]);
+      handleSelectionChange(newSelection);
+    }
+  }, [
+    bookmarkCollectionItems,
+    collections,
+    handleSelectionChange,
+    isOpen,
+    selectedKeys,
+  ]);
+
+  useEffect(() => {
+    if (!isOpen) {
+      setSelectedKeys(null);
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (
+      isOpen &&
+      bookmarkCollectionItems &&
+      bookmarkCollectionItems.length > 0
+    ) {
+      setSelectedKeys(
+        new Set(
+          bookmarkCollectionItems?.map((item) => item.expand!.collection!.id)
+        )
+      );
+    }
+  }, [isOpen, bookmarkCollectionItems]);
 
   return (
-    <Popover placement="bottom">
-      <PopoverTrigger
-        className={
-          bookmarkCollectionItems && bookmarkCollectionItems.length > 0
-            ? 'text-primary border-primary'
-            : 'text-secondary border-secondary'
-        }
+    <>
+      <Popover
+        placement="bottom"
+        isOpen={isOpen}
+        onOpenChange={(open) => setIsOpen(open)}
       >
-        {trigger}
-      </PopoverTrigger>
-      <PopoverContent className="py-2">
-        {collections && collections.length > 0 ? (
-          <Select
-            label="Collection"
-            placeholder="No collection selected"
-            className="w-56"
-            isLoading={isPending}
-            selectedKeys={selectedKeys}
-            onSelectionChange={handleSelectionChange}
-          >
-            {collections.map((collection) => (
-              <SelectItem key={collection.id} value={collection.id}>
-                {collection.title}
-              </SelectItem>
-            ))}
-          </Select>
-        ) : (
-          <div className="p-3">No collections created yet</div>
-        )}
-      </PopoverContent>
-    </Popover>
+        <PopoverTrigger
+          className={
+            bookmarkCollectionItems && bookmarkCollectionItems.length > 0
+              ? 'text-primary border-primary'
+              : 'text-secondary border-secondary'
+          }
+        >
+          {trigger}
+        </PopoverTrigger>
+        <PopoverContent className="py-2">
+          {collections && collections.length > 0 ? (
+            <Select
+              label="Collection"
+              placeholder="No collection selected"
+              className="w-56"
+              isLoading={isPending}
+              selectedKeys={selectedKeys ?? new Set([])}
+              onSelectionChange={handleSelectionChange}
+            >
+              {collections.map((collection) => (
+                <SelectItem key={collection.id} value={collection.id}>
+                  {collection.title}
+                </SelectItem>
+              ))}
+            </Select>
+          ) : (
+            <div className="p-3">No collections created yet</div>
+          )}
+        </PopoverContent>
+      </Popover>
+    </>
   );
 }
