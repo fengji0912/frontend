4b7d0dd806249aa1b606163a6de8095866c2936d
Allard Oelen <oelen@l3s.de>
2024-07-22 16:31:19 +0200

    fix: issue with items with a dot in the title

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,6 +16,7 @@ export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
+  // - Make exception for /item/[id]/[slug] where the slug can contain a dot
   // See: https://next-intl-docs.vercel.app/docs/routing/middleware#matcher-no-prefix
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)', '/([\\w-]+)?/item/(.+)'],
 };
